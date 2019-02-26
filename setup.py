1abbe8c9d89163c6c4ebc54de1f2651f16227b87
Miroslav Franc <miroslav.franc@nic.cz>
2019-02-26 14:38:54 +0100

    Ticket #21315 - fix licenses after a rebase

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2015-2018  CZ.NIC, z. s. p. o.
+# Copyright (C) 2015-2019  CZ.NIC, z. s. p. o.
 #
 # This file is part of FRED.
 #
--- webwhois/views/__init__.py
+++ webwhois/views/__init__.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2015-2018  CZ.NIC, z. s. p. o.
+# Copyright (C) 2015-2019  CZ.NIC, z. s. p. o.
 #
 # This file is part of FRED.
 #
@@ -21,10 +21,8 @@ Webwhois views.
 
 isort:skip_file
 """
-
 from __future__ import unicode_literals
 
-from .form_whois import WhoisFormView
 from .detail_contact import ContactDetailMixin, ContactDetailView
 from .detail_keyset import KeysetDetailMixin, KeysetDetailView
 from .detail_nsset import NssetDetailMixin, NssetDetailView
