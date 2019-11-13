1b9cc950ee7137d306f4d3e843867cc15abdbd9d
Vlastimil Zíma <vlastimil.zima@nic.cz>
2019-11-13 11:33:08 +0100

    Ticket #26355 - Update tox

--- test_cfg/settings.py
+++ test_cfg/settings.py
@@ -27,5 +27,6 @@ SECRET_KEY = 'SECRET'
 STATIC_URL = '/static/'
 
 WEBWHOIS_CORBA_EXPORT_MODULES = ('Registry', 'ccReg')
+WEBWHOIS_LOGGER = None
 WEBWHOIS_DNSSEC_URL = "http://www.nic.cz/dnssec/"  # ths can be None
 WEBWHOIS_CAPTCHA_MAX_REQUESTS = 100
