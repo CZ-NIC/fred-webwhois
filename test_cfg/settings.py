8cf2b5708d621807ed34d946397ac0fdc103cda7
Vlastimil Zíma <vlastimil.zima@nic.cz>
2019-11-15 14:18:15 +0100

    Ticket #26374 - Clean logger setting

--- test_cfg/settings.py
+++ test_cfg/settings.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2016-2018  CZ.NIC, z. s. p. o.
+# Copyright (C) 2016-2019  CZ.NIC, z. s. p. o.
 #
 # This file is part of FRED.
 #
@@ -27,6 +27,5 @@ SECRET_KEY = 'SECRET'
 STATIC_URL = '/static/'
 
 WEBWHOIS_CORBA_EXPORT_MODULES = ('Registry', 'ccReg')
-WEBWHOIS_LOGGER = 'pylogger.corbalogger.Logger'
 WEBWHOIS_DNSSEC_URL = "http://www.nic.cz/dnssec/"  # ths can be None
 WEBWHOIS_CAPTCHA_MAX_REQUESTS = 100
